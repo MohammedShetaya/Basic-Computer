426f620fb1adc02446f040aa7abb0cf2344ee55a
Shetaya <47546170+MohammedShetaya@users.noreply.github.com>
2021-06-21 22:14:24 +0200

    Update Instruction.java

--- src/Instruction.java
+++ src/Instruction.java
@@ -1,5 +1,6 @@
 public class Instruction {
     int binInstruction;
+    String stringInstruction;
 
     int opcode ; // bits31:26
     int r1 ; // bits25:21
@@ -15,12 +16,16 @@ public class Instruction {
 
     int aluResult ;
 
+    boolean jumpFlag;
+
     public Instruction(){
 
 
     }
     public void fetch(int binInstruction){
+
         this.binInstruction = binInstruction;
+        this.stringInstruction = convertBinaryToAssembly(binInstruction);
     }
 
     public void decode(int[] registerFile){
@@ -28,7 +33,7 @@ public class Instruction {
 //		System.out.println(instruction+" "+inst);
 
         // Rtype
-        Integer.parseInt(inst.substring(0, 4), 2);
+        opcode = Integer.parseInt(inst.substring(0, 4), 2);
         r1 = Integer.parseInt(inst.substring(4, 9), 2);
         r2 = Integer.parseInt(inst.substring(9, 14), 2);
         r3 = Integer.parseInt(inst.substring(14, 19), 2);
@@ -44,6 +49,9 @@ public class Instruction {
         valueR1 = registerFile[r1];
         valueR2 = registerFile[r2];
         valueR3 = registerFile[r3];
+
+        // string instruction
+        stringInstruction = convertBinaryToAssembly(binInstruction);
     }
 
     public void execute(int pc){
@@ -56,16 +64,20 @@ public class Instruction {
             case 2: aluResult = valueR2 * valueR3; break;
             // MOVI: R1 = IMM
             case 3: aluResult = imm; break;
-
+            // JEQ R1 R2 IMM
             // jump to  PC+1+IMM no registers needed
             // pc is already pointing to next instruction,add imm to it
-            case 4 : if( valueR1 == valueR2 ) aluResult= pc+imm; else aluResult = pc ;break ;
+            case 4 : if( valueR1 == valueR2 ){
+                aluResult= pc+imm;
+                jumpFlag = true;
+            }
+            else aluResult = pc ;break ;
             // R1 = R2 & R3
             case 5 : aluResult = valueR2 & valueR3; break;
             // R1 = R2 ⊕ IMM
             case 6 : aluResult = valueR2 ^ imm ; break ;
             // jump to address no registers needed
-            case 7 : aluResult = (pc & (15<<28)) + address ; break;
+            case 7 : aluResult = (pc & (15<<28)) + address ; jumpFlag = true; break;
            // logical shift left:R1 = R2<<<shamt
             case 8 :aluResult = valueR2<<shamt; break;
             // logical shift right: R1 = R2>>>shamt
@@ -82,6 +94,50 @@ public class Instruction {
             case 10: valueR1 = memory[aluResult]; break;
             //MOVM = MEM[R2+IMM]=R1
             case 11: memory[aluResult] = valueR1; break;
+        }
+    }
+
+    public String convertBinaryToAssembly(int instruction) {
+        String inst = numberTobinaryString(instruction);
+        // Rtype
+        int opcode = Integer.parseInt(inst.substring(0, 4), 2);
+        int r1 = Integer.parseInt(inst.substring(4, 9), 2);
+        int r2 = Integer.parseInt(inst.substring(9, 14), 2);
+        int r3 = Integer.parseInt(inst.substring(14, 19), 2);
+        int shamt = Integer.parseInt(inst.substring(19, 32), 2);
+
+        // Immediate
+        int imm = Integer.parseInt(inst.substring(14, 32), 2);
+
+        // Jtype
+        int address = Integer.parseInt(inst.substring(4, 32), 2);
+        switch (opcode){
+            //add: R1 = R2 + R3
+            case 0: return ("ADD " + "R" + r1 + " R" + r2 + " R" + r3);
+            // sub: R1 = R2 - R3
+            case 1: return ("SUB " + "R" + r1 + " R" + r2 + " R" + r3);
+            // mul: R1 = R2 * R3
+            case 2: return ("MUL " + "R" + r1 + " R" + r2 + " R" + r3);
+            // MOVI: R1 = IMM
+            case 3: return ("MOVI " + "R" + r1 + " " + imm);
+            // jump to  PC+1+IMM no registers needed
+            // pc is already pointing to next instruction,add imm to it
+            case 4: return ("JEQ " + "R" + r1 + " R" + r2 + " " + imm);
+            // R1 = R2 & R3
+            case 5: return ("AND " + "R" + r1 + " R" + r2 + " R" + r3);
+            // R1 = R2 ⊕ IMM
+            case 6: return ("XORI " + "R" + r1 + " R" + r2 + " " + imm);
+            // jump to address no registers needed
+            case 7: return ("JMP " + address);
+            // logical shift left:R1 = R2<<<shamt
+            case 8: return ("LSL " + "R" + r1 + " R" + r2 + " " + shamt);
+            // logical shift right: R1 = R2>>>shamt
+            case 9: return ("LSR " + "R" + r1 + " R" + r2 + " " + shamt);
+            // MOVR = R1= MEM[R2+IMM]
+            case 10: return("MOVR" + "R" + r1 + " R" + r2 + " " + imm);
+            //MOVM = MEM[R2+IMM]=R1
+            case 11: return("MOVM " + "R" + r1 + " R" + r2 + " " + imm);
+            default: return "";
         }
     }
 
