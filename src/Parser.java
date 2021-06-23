44cdc96da71849a94655e7b627e6a3f10ffd6652
Omar Khair <omarkhair66@gmail.com>
2021-06-23 12:26:31 +0200

    add comments and review Parser class

--- src/Parser.java
+++ src/Parser.java
@@ -36,7 +36,11 @@ public class Parser {
                     opCode = 9;
                     break;
             }
+            // R-format
+            // opcode(4) + R1(5) + R2(5) + R3(5) + SHAMT(13)
+
             binaryInstruction += firstNBits(opCode, 4);
+
             for (int i = 1; i <= 2; i++) {
                 int regIdx = Integer.parseInt(term[i].substring(1));
                 binaryInstruction += firstNBits(regIdx, 5);
@@ -70,12 +74,16 @@ public class Parser {
                     opCode = 11;
                     break;
             }
+            // I-format
+            // opcode(4) + R1(5) + R2(5) + IMM(18)
+
             binaryInstruction += firstNBits(opCode, 4);
-            int regIdx = Integer.parseInt(term[1].substring(1));
-            binaryInstruction += firstNBits(regIdx, 5);
+            int r1 = Integer.parseInt(term[1].substring(1));
+            binaryInstruction += firstNBits(r1, 5);
             int r2 = 0;
             int imm = 0;
             // MOVR and MOVM format are changed to MOVR R1 R2 IMM
+
             /*
             if (term[0].equals("MOVR") || term[0].equals("MOVM")) {
                 for (int i = 0; i < term[2].length(); i++) {
@@ -88,6 +96,7 @@ public class Parser {
                 }
             }
             */
+
             if (term[0].equals("MOVI")){
                 imm = Integer.parseInt(term[2]) ;
             }
@@ -95,15 +104,19 @@ public class Parser {
                 r2 = Integer.parseInt(term[2].substring(1)) ;
                 imm = Integer.parseInt(term[3]) ;
             }
+
             binaryInstruction += firstNBits(r2 , 5 ) ;
             binaryInstruction += firstNBits(imm , 18) ;
 
         } else {
+            // J-Type instruction
+            // opcode(4) + address(28)
             opCode = 7;
             binaryInstruction += firstNBits(opCode, 4);
             int address = Integer.parseInt(term[1]);
-            binaryInstruction += firstNBits(address, 2);
+            binaryInstruction += firstNBits(address, 28);
         }
+
         return Integer.parseInt(binaryInstruction, 2);
     }
 
@@ -130,6 +143,8 @@ public class Parser {
         return -1;
     }
 
+
+    // returns the right-most n-bits from the number num
     private String firstNBits(int num, int n) {
         StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
@@ -138,21 +153,15 @@ public class Parser {
         }
         return sb.reverse().toString();
     }
+
+    // this method returns the 32 bit binary string of the integer x
     public static String toBinaryString(int x){
         String s= Integer.toBinaryString(x);
         while (s.length() < 32 ) {
-            s = '0' + s ;
+            s = s.charAt(0) + s ;
         }
         return s;
 
     }
 
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        while(true){
-//            String s = sc.nextLine();
-//
-//            System.out.println(toBinaryString(convertAssemblyToBinary(s)));
-//        }
-//    }
 }
